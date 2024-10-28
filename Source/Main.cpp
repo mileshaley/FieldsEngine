5f882c71a96d9107e1b3124b45cd7525ac15ecb6
Miles Haley <71760141+mileshaley@users.noreply.github.com>
2024-10-28 07:11:02 -0700

    added basic libraries

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,14 @@
 
+#include <iostream>
+#include "imGui/imgui.h"
+#include "json-develop/single_include/nlohmann/json.hpp"
+
+using Json = nlohmann::json;
 
 int main() {
-
+	Json json;
+	json["twelve"] = 5;
+	std::cout << json << std::endl;
+	std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
