abfbc22f9285ff0f1e902d95ddd41a835c8beeef
Miles Haley <71760141+mileshaley@users.noreply.github.com>
2024-11-22 15:47:18 -0800

    added SDL3 and editor configurations

--- source/main.cpp
+++ source/main.cpp
@@ -9,6 +9,7 @@
 #include "imGui/imgui.h"
 #include "application.h"
 #include "context.h"
+#include "SDL3/SDL.h"
 
 int main() {
 	using namespace fields_engine;
@@ -16,13 +17,29 @@ int main() {
 	//return 0;
 
 	std::cout << "Fields Engine Startup" << std::endl;
+
+	if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
+	{
+		printf("Error: SDL_Init(): %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("SDL TEST", 1280, 720, 0);
+	if (window == nullptr)
+	{
+		printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+		return -1;
+	}
+	SDL_Delay(3000); // Wait 3 seconds
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	{
 		local_context<application> app{};
 		if (!app->startup()) {
 			return 1;
 		}
 		app->run();
-
+	
 		if (!app->shutdown()) {
 			return 1;
 		}
